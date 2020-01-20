bd73d260ab772675b03e6b745e41e83610a512f9
efrontin <fe.moustic@gmail.com>
2020-01-20 14:44:38 +0100

    [VIEW] Add first view + gitignore

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        result['nom'] = 'Evrard'
+
+        return result
+
+    template_name = 'index.html'
