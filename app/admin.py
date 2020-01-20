e126b63f4da65d2f695f96a84c71f0b596124ca3
efrontin <fe.moustic@gmail.com>
2020-01-20 14:09:45 +0100

    [BDD] Ajout des migrations

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import User, Project, Status, Level, Company, Ticket, Role, CompanyProject
+
+
+class CompanyProjectInlineAdmin(admin.StackedInline):
+    model = CompanyProject
+    # permet de rechercher le tag dans champs de recherche
+    extra = 0
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = (CompanyProjectInlineAdmin, )
+
+
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Company)
+admin.site.register(Level)
+admin.site.register(Role)
+admin.site.register(Status)
+admin.site.register(Project, ProjectAdmin)
+
--- app/models.py
+++ app/models.py
@@ -5,53 +5,76 @@ class User(models.Model):
     name = models.CharField(max_length=50,
                             blank=False,
                             null=False)
-    role = models.CharField(max_length=50,
-                            blank=False,
-                            null=False)
-    password = models.TextField(blank=True,
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+    password = models.CharField(max_length=55,
+                                blank=True,
                                 null=True)
 
-    companies = models.ManyToManyField('Company', through='UserCompany')
+    companies = models.ForeignKey('Company', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, )
 
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, )
 
-    is_client = models.BooleanField(blank=False)
+    class Meta:
+        verbose_name_plural = 'Companies'
 
+    def __str__(self):
 
-class UserCompany(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    company = models.ForeignKey( Company, on_delete=models.CASCADE)
+        return f'{self.name}'
 
 
 class Level(models.Model):
-    label = models.CharField
+    label = models.CharField(max_length=55,
+                             default='No level')
+
+    def __str__(self):
+        return f'{self.label}'
 
 
 class Status(models.Model):
-    label = models.CharField
+    label = models.CharField(max_length=55,
+                             default='No Status')
+
+    def __str__(self):
+        return f'{self.label}'
+
+    class Meta:
+        verbose_name_plural = 'Status'
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=55)
+    name = models.CharField(max_length=55, )
 
     created_at = models.DateTimeField
 
-    status = models.ForeignKey('Status',
-                               on_delete=models.CASCADE)
+    company = models.ManyToManyField(Company, through='CompanyProject', related_name='projects')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=55,
+                            default='No role')
+
+    class Meta:
+        verbose_name_plural = 'Roles'
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Ticket(models.Model):
-    label = models.CharField
+    label = models.TextField(max_length=255,
+                             default='No description')
 
-    user_client = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField('User')
 
     created_at = models.DateTimeField
 
@@ -63,16 +86,22 @@ class Ticket(models.Model):
     level = models.ForeignKey(Level,
                               on_delete=models.CASCADE)
 
-    user_dev = models.ForeignKey(User, on_delete=models.CASCADE)
-
     project = models.ForeignKey(Project,
                                 on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.label}'
 
-class TicketUser(models.Model):
-    ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class CompanyProject(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    is_client = models.BooleanField(blank=False)
 
     def __str__(self):
-        pass
+        if self.is_client:
+            return f'{self.company} a effectué une demande'
+        return f'{self.company} répond à une demande '
 
