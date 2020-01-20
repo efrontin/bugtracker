33ccc0c8832d95536fd70ccfb5ab9508d321b959
efrontin <fe.moustic@gmail.com>
2020-01-20 16:13:14 +0100

    Ajout nouvelle route + index

--- app/views.py
+++ app/views.py
@@ -3,11 +3,14 @@ from django.views import generic
 
 
 # Create your views here.
+from app.models import Project
+
 
 class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         result = super().get_context_data(**kwargs)
         result['nom'] = 'Evrard'
+        result['projects'] = Project.objects.all().order_by('name')
 
         return result
 
